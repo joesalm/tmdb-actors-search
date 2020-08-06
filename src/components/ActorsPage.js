f82efe453bb028e05b04b7eb88235906546f5503
nirch <nirh2@yahoo.com>
2020-08-06 09:34:45 +0300

    Adding an empty Actors Page

---
+++ src/components/ActorsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class ActorsPage extends React.Component {
+
+    render() {
+        return (
+            <div className="p-actors">
+                Actors Page
+            </div>
+        )
+    }
+}
+
+export default ActorsPage;
